05ad8a29b4a2474e6cc4f7f76c76e77e01b08744
Giorgio Momigliano <gmomigliano@protonmail.com>
2023-01-23 13:52:58 +0100

    Remove useless imports

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/platelminto/NudeNetClassifier'
 EMAIL = 'gmomigliano@protonmail.com'
 AUTHOR = 'Giorgio Momigliano'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.1'
+VERSION = '2.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
